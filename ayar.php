fc3987bf0174eca46de97393e48ce26fa1855e87
Mustafa Mete <54574307+Mustafazxcv@users.noreply.github.com>
2023-12-02 17:16:35 +0300

    ayar.php

--- ayar.php
+++ ayar.php
@@ -1,6 +1,6 @@
 ```
 <?php
-$host = 'localhost'; // Veritabanı sunucusunun adresi
+$host = 'localhost'; // Veritabanı sunucusunun adresi çogunluklu olarak 3306 portu üzerinden bağlantı sağlanır istenilecek şekilde localhost:3306 yazılabilir.
 $dbname = 'örnek1_veritabanı'; // Veritabanı adı
 $username = 'örnek1_admin'; // Veritabanı kullanıcı adı
 $password = 'güçlü1parola_A'; // Veritabanı parolası
